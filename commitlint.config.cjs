338b4507f2cefd1a4a3249ea6e74bae2a0e4f280
zhaoguangxu <zhaoguangxu801@gmail.com>
2025-06-03 01:20:34 +0800

    chore: 配置git提交规范

---
+++ commitlint.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // 新功能
+        'fix', // 修复bug
+        'docs', // 文档更新
+        'style', // 代码格式（不影响代码运行的变动）
+        'refactor', // 重构（既不是新增功能，也不是修改bug的代码变动）
+        'perf', // 性能优化
+        'test', // 增加测试
+        'chore', // 构建过程或辅助工具的变动
+        'revert', // 回滚
+        'build', // 打包
+        'ci', // CI配置相关
+      ],
+    ],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'subject-full-stop': [0],
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72],
+  },
+};
